036e9bff23679da7898650393fe64350171873a4
joaquinxtomas <jtppalacios@gmail.com>
2024-10-17 21:49:58 -0300

    first commit

---
+++ spring-deploy/README.md
@@ -0,0 +1,4 @@
+Crear archivo 'system.properties' y colocar la version de tu java para subir a github y que tu app de despliegue lo detecte.
++ <h3>java.runtime.version = 17
+
+Luego configurar git y realizar un commit and push.
---
+++ spring-deploy/src/main/java/com/example/spring_deploy/HelloController.java
@@ -0,0 +1,43 @@
+package com.example.spring_deploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Joaquin Palacios
+ */
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")
+    String message;
+
+    @GetMapping("/hola")
+    public String holaMundo(){
+        System.out.println(message);
+        return "hola mundo que tal como vais hasta luego tu";
+    }
+
+    @GetMapping("/holalindo")
+    public String bootstrap(){
+        return """
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Bootstrap demo</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Hello, world!</h1>
+                    <a class = "btn btn-primary" href="https://google.com">Google</a>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                  </body>
+                </html>
+                """;
+    }
+
+}
