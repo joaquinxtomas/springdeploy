e16392a58388eef4d6715c316c62c6bf19abfb6a
joaquinxtomas <jtppalacios@gmail.com>
2024-11-30 17:23:51 -0300

    first commit to deploy

---
+++ springdep/src/main/java/com/springdeploy/springdep/SpringdepApplication.java
@@ -0,0 +1,13 @@
+package com.springdeploy.springdep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdepApplication.class, args);
+	}
+
+}
---
+++ springdep/src/test/java/com/springdeploy/springdep/SpringdepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdeploy.springdep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
